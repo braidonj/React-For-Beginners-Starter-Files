62b522165c40b1bd89ec78c2bdb93928b9a597c5
braidonj <47284747+braidonj@users.noreply.github.com>
2020-04-01 14:47:57 -0600

    Finished video 10

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => (
+  <div>
+    <h2>
+      Page Not Found{" "}
+      <span role="img" aria-label="frowning face">
+        ☹️
+      </span>
+    </h2>
+  </div>
+);
+
+export default NotFound;
